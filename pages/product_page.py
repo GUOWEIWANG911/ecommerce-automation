d56d45decbb0f29c459901c70119486702d1e964
Acho <yc_wgw@163.com>
2026-07-22 23:45:32 +0800

    Debug : Add print log info

--- pages/product_page.py
+++ pages/product_page.py
@@ -10,17 +10,20 @@ class ProductPage(BasePage):
     ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, "a[href*='addItemToCart']")
 
     def add_to_cart(self):
+        print(self.driver.current_url)
+
         """将商品加入购物车"""
         # 2. 把等待和点击逻辑放进方法里
-        wait = WebDriverWait(self.driver, 20)
+        wait = WebDriverWait(self.driver, 30)
         
         # 先确认页面加载到了商品列表区域
         wait.until(EC.presence_of_element_located((By.ID, "Catalog")))
         
         # 再等待按钮可点击
         add_to_cart_btn = wait.until(
-            EC.element_to_be_clickable(self.ADD_TO_CART_BUTTON)
+            EC.visibility_of_element_located(self.ADD_TO_CART_BUTTON)
         )
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", add_to_cart_btn)
         add_to_cart_btn.click()
         
         # 3. 返回购物车页面对象
