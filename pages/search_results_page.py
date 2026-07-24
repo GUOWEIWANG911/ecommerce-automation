cec0656ab7f817adb48e6f444514912477c9b7f3
Acho <yc_wgw@163.com>
2026-07-25 02:11:05 +0800

    Add log info

--- pages/search_results_page.py
+++ pages/search_results_page.py
@@ -15,13 +15,16 @@ class SearchResultsPage:
 
         first_product_link = (By.XPATH, "//div[@id='Catalog']//tr[2]//td[1]/a[contains(@href, 'viewProduct')]")
         
-        wait = WebDriverWait(self.driver, 20)
+        wait = WebDriverWait(self.driver, 30)
         
         # 等待链接出现并可点击
         link = wait.until(EC.element_to_be_clickable(first_product_link))
         link.click()
+
+        print(f"[DEBUG] 点击后，当前页面URL: {self.driver.current_url}")
+        print(f"[DEBUG] 点击后，当前页面标题: {self.driver.title}")
         
         # 等待页面跳转到商品详情页
         wait.until(EC.presence_of_element_located((By.XPATH, "//a[text()='Add to Cart']")))
-        
+        print(f"[DEBUG] 商品详情页已加载: {self.driver.current_url}")
         return ProductPage(self.driver)
